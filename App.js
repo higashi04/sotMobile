1c18864a00cd0c8981b6a5c78fbef14f9ef9dcaf
Cesar <cesar.higashi04@gmail.com>
2022-05-03 16:48:18 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/272707556_231916139148938_3564323865832014711_n-removebg-small.png')} />
+      <Text style={styles.text}>Transportes Villarreal</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#669BC7',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 50
+  }
 });
